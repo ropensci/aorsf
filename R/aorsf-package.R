292768b0f6135862c636ddb1f095ba9d4bc823a3
bcjaeger <byron.jaeger@gmail.com>
2022-07-19 06:59:01 -0400

    multiple points of entry
    The package doc page is now accessible and the package logo appears in
    the website, the doc page, and the readme.

--- R/aorsf-package.R
+++ R/aorsf-package.R
@@ -1,10 +1,10 @@
 #' @keywords internal
+#' @import data.table
 "_PACKAGE"
 
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
-#' @import data.table
 #' @importFrom Rcpp sourceCpp
 #' @useDynLib aorsf, .registration = TRUE
 ## usethis namespace: end
