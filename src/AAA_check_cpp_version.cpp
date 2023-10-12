25b1e5f1b7c5f8855578e051b345517aad348f6b
bjaeger <bjaeger@wakehealth.edu>
2023-10-12 08:49:44 -0400

    cpp14 checks

---
+++ src/AAA_check_cpp_version.cpp
@@ -0,0 +1,5 @@
+#ifndef WIN_R_BUILD
+#if __cplusplus < 201402L
+#error Error: aorsf requires a C++14 compiler, e.g., gcc >= 5 or Clang >= 3.4. You probably have to update your C++ compiler.
+#endif
+#endif
