523272790d50c8e4a024826009ede26dbb8b0576
byron jaeger <byron.jaeger@gmail.com>
2023-10-21 16:07:39 -0400

    delayed fill of pd values

--- tests/testthat/test-DataCpp.R
+++ tests/testthat/test-DataCpp.R
@@ -16,9 +16,50 @@ test_that(
                                                  x_cols = x_cols - 1,
                                                  beta = beta)
 
+  # won't be used
+  pd_x_vals <- matrix(1)
+  pd_x_cols <- ncol(pbc_mats$x)
+
+  data_cpp_answer_2 <- x_submat_mult_beta_pd_exported(x = pbc_mats$x,
+                                                      y = pbc_mats$y,
+                                                      w = pbc_mats$w,
+                                                      x_rows = x_rows - 1,
+                                                      x_cols = x_cols - 1,
+                                                      beta = beta,
+                                                      pd_x_vals = pd_x_vals,
+                                                      pd_x_cols = pd_x_cols)
+
   target <- pbc_mats$x[x_rows, x_cols] %*% beta
 
   expect_equal(data_cpp_answer, target)
+  expect_equal(data_cpp_answer_2, target)
 
  })
+
+test_that(
+ desc = "submatrix multiplication with PD values is correct",
+ code = {
+
+  pd_x_vals <- c(0, 0)
+  pd_x_cols <- x_cols[1:2] - 1
+
+  data_cpp_answer_2 <- x_submat_mult_beta_pd_exported(x = pbc_mats$x,
+                                                      y = pbc_mats$y,
+                                                      w = pbc_mats$w,
+                                                      x_rows = x_rows - 1,
+                                                      x_cols = x_cols - 1,
+                                                      beta = beta,
+                                                      pd_x_vals = pd_x_vals,
+                                                      pd_x_cols = pd_x_cols)
+
+  x_pd <- pbc_mats$x
+  x_pd[, x_cols[c(1,2)]] <- 0
+
+  target <- x_pd[x_rows, x_cols] %*% beta
+
+  expect_equal(data_cpp_answer_2, target)
+
+
+ }
+)
 
