0ee97c4eae3de20141870b9a66e197c0951bb512
byron jaeger <byron.jaeger@gmail.com>
2023-11-17 11:21:01 -0500

    skip snaps, call data for cars

--- tests/testthat/test-verbosity.R
+++ tests/testthat/test-verbosity.R
@@ -4,6 +4,8 @@ test_that(
  desc = 'verbosity prints grow, predict, and importance notes',
  code = {
 
+  skip_on_ci()
+
   expect_snapshot(
    fit_verbose <- orsf(pbc, time + status ~.,
                        verbose_progress = TRUE,
